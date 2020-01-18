8070476e3e717a79f25fdabe4d69be43ac9a2c76
Ahmad Alfy <ahmadalfy@gmail.com>
2020-01-19 01:06:25 +0200

    refactor: switch to async/await

--- scripts/data-handler.js
+++ scripts/data-handler.js
@@ -15,35 +15,32 @@ class DataSource {
 		DataSource.instance = this;
 	}
 
-	fetchGroups() {
-		return Groups.load().then(groups => {
-			this.data.groups = groups;
-			db.groups.bulkPut(groups);
-			localStorage.setItem('groups', Date.now());
-			return groups;
-		})
+	async fetchGroups() {
+		const groups = await Groups.load();
+		this.data.groups = groups;
+		db.groups.bulkPut(groups);
+		localStorage.setItem('groups', Date.now());
+		return groups;
 	}
 
-	fetchProjects() {
-		return Projects.load(this.data.groups).then(projects => {
-			this.data.projects = projects.flat();
-			this.data.projects.forEach(project => {
-				project.group_id = project.namespace.id;
-			});
-			db.projects.bulkPut(this.data.projects);
-			localStorage.setItem('projects', Date.now());
-			return projects;
-		})
-	}
-
-	fetchMembers() {
-		return Members.load(this.data.groups).then(members => {
-			const key = 'id';
-			this.data.members = [...new Map(members.flat().map(item =>[item[key], item])).values()];
-			db.members.bulkPut(this.data.members);
-			localStorage.setItem('members', Date.now());
-			return members;
+	async fetchProjects() {
+		const projects = await Projects.load(this.data.groups);
+		this.data.projects = projects.flat();
+		this.data.projects.forEach(project => {
+			project.group_id = project.namespace.id;
 		});
+		db.projects.bulkPut(this.data.projects);
+		localStorage.setItem('projects', Date.now());
+		return projects;
+	}
+
+	async fetchMembers() {
+		const members = await Members.load(this.data.groups);
+		const key = 'id';
+		this.data.members = [...new Map(members.flat().map(item => [item[key], item])).values()];
+		db.members.bulkPut(this.data.members);
+		localStorage.setItem('members', Date.now());
+		return members;
 	}
 }
 
