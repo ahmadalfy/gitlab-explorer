4f7c3a006a14ace76a3249b8a44511bf3cc22967
Eslam Hamouda <eslamx7@gmail.com>
2020-02-02 00:26:47 +0200

    feat: Add support for Gitlab self-hosted servers

--- env.sample.js
+++ env.sample.js
@@ -1,3 +1,6 @@
-const token = '';
+const env = {
+	token: '',
+	baseUrl: 'https://gitlab.com', // Looking for self-hosted support? Change this value in your env.js file to match your Gitlab installation domain
+}
 
-export default token;
+export default env;
