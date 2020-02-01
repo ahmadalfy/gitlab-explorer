a660e8e87f85f5ab8a4475d23cada49b1e7bb887
Eslam Hamouda <eslamx7@gmail.com>
2020-02-02 00:26:47 +0200

    add support for Gitlab self-hosted servers, closes #16

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,12 +1,12 @@
-import token from '../env.js';
+import env from '../env.js';
 
 class Utilities {
 	static async req(api, qs, method = 'GET', headers = {}) {
-		const response = await fetch(`https://gitlab.com/api/v4/${api}?per_page=500`, {
+		const response = await fetch(`${env.baseUrl}/api/v4/${api}?per_page=500`, {
 			method,
 			headers: {
 				...headers,
-				'PRIVATE-TOKEN': token,
+				'PRIVATE-TOKEN': env.token,
 			}
 		});
 		return await response.json();
